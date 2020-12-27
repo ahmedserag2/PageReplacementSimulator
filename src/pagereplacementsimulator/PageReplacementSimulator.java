c3cbe8142abc95a7a54c08df7d071bf0edd02476
ahmed yehia <ahmedyehia2576@gmail.com>
2020-12-28 01:27:30 +0200

    Main

--- src/pagereplacementsimulator/PageReplacementSimulator.java
+++ src/pagereplacementsimulator/PageReplacementSimulator.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package pagereplacementsimulator;
+import java.util.Scanner;
 
 /**
  *
@@ -16,12 +17,56 @@ public class PageReplacementSimulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int noFrames = 3;
-        String refrenceString1 = "1303563";
-        String refrenceString2  = "012301401234";
-        PageAllocator table1 = new LRUAllocator(noFrames , refrenceString1);
-        table1.allocate();
-        table1.printTable();
+        Scanner MyInput = new Scanner(System.in);
+        Scanner MyInput1 = new Scanner(System.in);
+        String Resume = "";
+       
+        
+        do{
+            
+            System.out.println("Enter your refrence of string");
+            String RefString = MyInput1.nextLine();
+            System.out.println("\n");
+   
+            System.out.println("Enter number of frames");
+            int Frames = MyInput.nextInt();
+            System.out.println("\n");
+            
+            PageAllocator table1 = new FifoAllocator(Frames , RefString);
+            PageAllocator table2 = new LRUAllocator(Frames , RefString);
+            PageAllocator table3 = new OptimalAllocator(Frames , RefString);
+            
+            System.out.println("Algorithm menu");
+            PageAllocator.DisplayMenu();
+            System.out.println("\n");
+            
+            System.out.println("Choose which algorithm you want to apply");
+            int choice=MyInput.nextInt();
+            System.out.println("\n");
+            
+            switch (choice) {
+                case 1:         
+                    table1.allocate();
+                    table1.printTable();
+                    break;
+                case 2: 
+                    table2.allocate();
+                    table2.printTable();
+                    break;
+                case 3: 
+                    table3.allocate();
+                    table3.printTable();
+                    break;
+                default: System.out.println("Invalid choice");
+            }
+
+            
+            System.out.println("Do you want to make another operation y or n");
+            Resume = MyInput1.nextLine();
+            System.out.println("\n");
+
+        }while(Resume.equalsIgnoreCase("Y"));
+        
     }
     
 }
